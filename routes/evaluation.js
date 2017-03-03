20b3fdc51154f880bd2898d92de4faf840c6e804
Emanuel Arias <arias.emanuel@gmail.com>
2017-03-03 16:10:56 -0300

    added secutity to routes

--- routes/evaluation.js
+++ routes/evaluation.js
@@ -8,16 +8,12 @@ const router = express.Router();
 
 // do the automatic evaluation
 // router.get('/automatic_evaluation',mids.isLoggedIn, mids.roleAuthorization['admin'], evaluationCtrl.makeAutomaticEvaluation);
-// router.get('/automatic_evaluation', evaluationCtrl.makeAutomaticEvaluation);
 router.get('/automatic', evaluationCtrl.makeAutomaticEvaluation);
-router.get('/get/:slug', evaluationCtrl.getEvaluation);
 
 // manual evaluation view
-// router.get('/manual/:slug', mids.isLoggedIn, mids.roleAuthorization(['admin', 'evaluator']), evaluationCtrl.renderNewManualEvaluationView);
-router.get('/manual/:slug', evaluationCtrl.renderNewManualEvaluationView);
+router.get('/manual/:slug', mids.isLoggedIn, mids.roleAuthorization(['admin', 'evaluator']), evaluationCtrl.renderNewManualEvaluationView);
 
 // save manual evaluation
-// router.post('/manual/:slug', mids.isLoggedIn, mids.roleAuthorization(['admin', 'evaluator']),  evaluationCtrl.saveManualEvaluation);
-router.post('/manual/:slug', evaluationCtrl.saveManualEvaluation);
+router.post('/manual/:slug', mids.isLoggedIn, mids.roleAuthorization(['admin', 'evaluator']),  evaluationCtrl.saveManualEvaluation);
 
 module.exports = router;
